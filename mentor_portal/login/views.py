a4acbfa75c37addc5bc81f03434208022117bcb2
Brigham Andersen <brighamband@gmail.com>
2020-05-20 17:41:23 -0600

    Got semi-basic signup working.  Redirects to login page.

--- mentor_portal/login/views.py
+++ mentor_portal/login/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from login.forms import SignUpForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,7 +13,18 @@ def testing(request):
     return HttpResponse("Testing Basic HttpResponse.")
 
 def signup(request):
-    return render(request, "signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, "signup.html", {'form': form})
 
 def profile(request):
     return render(request, "myprofile.html")
