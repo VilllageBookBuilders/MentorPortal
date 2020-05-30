ff169715181e6f7352c0ba7feed26dee0cdb0261
Ed Ringger <edringger@gmail.com>
2020-05-30 16:50:36 -0700

    finished auth frontend

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
 import BaseRouter from './routes';
 import 'antd/dist/antd.css';
+import * as actions from './store/actions/auth';
 
 import CustomLayout from './containers/Layout';
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.onTryAutoSignup();
+    console.log(props);
+  });
   return (
     <div className="App">
       <Router>
         <div>
-          <CustomLayout>
+          <CustomLayout {...props}>
             <BaseRouter />
           </CustomLayout>
         </div>
@@ -20,4 +26,17 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+//if we didn't have map statetoprops, pass null in its place
