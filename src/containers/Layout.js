db87510bbe1a64e3c8e61e2e75f5daff542b1fbf
Brigham Andersen <brighamband@gmail.com>
2020-06-13 11:59:16 -0600

    Updated Layout to Match VBB (TODO:  Button and Page Sizing)

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,43 +1,24 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import * as actions from '../store/actions/auth';
+import '../Style.css'
+import logo from "./vbb-logo.png"
 
-const { Header, Content, Footer } = Layout;
-
-const CustomLayout = (props) => {
+const Layout = (props) => {
     return(
-        <Layout className="layout">
-            <Header>
-            <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                {
-                    props.isAuthenticated ?
-                    <Menu.Item key="2" onClick={ props.logout }>
-                        Logout
-                    </Menu.Item>
-                    :
-                    <Menu.Item key="2">
-                        <Link to="/login">Login</Link>
-                    </Menu.Item>
-                }
-                <Menu.Item key="1">
-                    <Link to="/">Posts</Link>
-                </Menu.Item>
-            </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item><Link to ='/'>Home</Link></Breadcrumb.Item>
-                <Breadcrumb.Item><Link to='/'>List</Link></Breadcrumb.Item>
-            </Breadcrumb>
-                <div className="site-layout-content">
-                    {props.children}
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
+        <body style={{backgroundColor: "#fffff6", padding: "50px"}}>
+            <header>
+                <img src={logo} alt='Test' style={{width: "400px"}} />
+                {/* <button type="button" style={{textAlign: 'center'}}>Click Me!</button> */}
+            </header>
+
+            <div className="site-layout-content">
+                {props.children}
+            </div>
+
+            <footer style={{textAlign: 'center'}}>Village Book Builders 2020</footer>
+        </body>
     );
 }
 
@@ -47,4 +28,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(CustomLayout));
+export default withRouter(connect(null, mapDispatchToProps)(Layout));
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,54 +1,53 @@
-import React from 'react';
+import React from "react";
+import "../Style.css";
 
 const ProfilePage = () => {
   return (
     <div>
-        <h1 style={{"fontFamily":"'Caveat', cursive","fontSize":"3em","color":"#4C4C49","paddingRight":"400px"}}>Welcome to Mentor Portal!</h1>
-        <br />
-        <br />
-        <h2 style={{ color: "#4C4C49" }}>My Weekly Mentoring Session</h2>
-        <h3 style={{ color: "#6AC66B" }}>Tuesdays @ 6pm MST</h3>
-        <hr style={{ border: "1px solid black" }} />
-        <h2 style={{ color: "#4C4C49" }}>Mentoring Resources</h2>
-        <a href="https://edu.google.com/intl/en/products/classroom/?modal_active=none&zippy-set-single_activeEl=zippy-single-set-8">
-          Google Classroom
-        </a>
-        <br />
-        <a href="https://apps.google.com/meet/">Google Meets</a>
-        <br />
-        <a href="#.html">Session Journal Entries</a>
-        <br />
-        <a href="#.html">Mentee Profile</a>
-        <br />
-        <a href="https://360.articulate.com/review/content/73bf3afe-47f9-4f9f-aa4d-70bf27fbe8d5/review">
-          Mentor Training
-        </a>
-        <br />
-        <br />
-        <hr style={{ border: "1px solid black" }} />
-        <h2 style={{ color: "#4C4C49" }}>My Info</h2>
-        <h5>View and edit your profile information</h5>
-        <h3 style={{ color: "#FF914D" }}>Account-Related</h3>
-        Name (first and last)
-        <br />
-        Email
-        <br />
-        Phone Number
-        <br />
-        Change Password
-        <br />
-        <h3 style={{ color: "#549BEA" }}>Mentor-Related</h3>
-        Languages I Speak
-        <br />
-        My Time Zone
-        <br />
-        My Availability to Mentor
-        <br />
-        <br />
+      <br />
+      <h1>Welcome to Mentor Portal!</h1>
+      <br />
+      <h2 style={{ color: "#4C4C49" }}>My Weekly Mentoring Session</h2>
+      <h3 style={{ color: "#6AC66B" }}>Tuesdays @ 6pm MST</h3>
+      <hr style={{ border: "1px solid black" }} />
+      <h2 style={{ color: "#4C4C49" }}>Mentoring Resources</h2>
+      <a href="https://edu.google.com/intl/en/products/classroom/?modal_active=none&zippy-set-single_activeEl=zippy-single-set-8">
+        Google Classroom
+      </a>
+      <br />
+      <a href="https://apps.google.com/meet/">Google Meets</a>
+      <br />
+      <a href="#.html">Session Journal Entries</a>
+      <br />
+      <a href="#.html">Mentee Profile</a>
+      <br />
+      <a href="https://360.articulate.com/review/content/73bf3afe-47f9-4f9f-aa4d-70bf27fbe8d5/review">
+        Mentor Training
+      </a>
+      <br />
+      <br />
+      <hr style={{ border: "1px solid black" }} />
+      <h2 style={{ color: "#4C4C49" }}>My Info</h2>
+      <h5>View and edit your profile information</h5>
+      <h3 style={{ color: "#FF914D" }}>Account-Related</h3>
+      Name (first and last)
+      <br />
+      Email
+      <br />
+      Phone Number
+      <br />
+      Change Password
+      <br />
+      <h3 style={{ color: "#549BEA" }}>Mentor-Related</h3>
+      Languages I Speak
+      <br />
+      My Time Zone
+      <br />
+      My Availability to Mentor
+      <br />
+      <br />
     </div>
-
   );
 };
-
 
 export default ProfilePage;
