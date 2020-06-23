a9fe7845ef3d7f9d1fc09aa52c172a5c92edf4b6
Brigham Andersen <brighamband@gmail.com>
2020-06-22 20:14:36 -0600

    Fixed nav bar to have text level with logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,12 +9,12 @@ const Layout = (props) => {
     return(
         <body style={{backgroundColor: "#fffff6" }}>
             <div>
-                <img src={logo} alt='Logo' style={{width: "300px", padding: "20px"}} />
-                <h1 style={{ display: "inline-block", paddingLeft: "100px" }}>Mentor Portal</h1>
-                <a href="https://www.villagebookbuilders.org/giftabook/" style={{ display: "inline-block", paddingLeft: "100px" }}><button>Donate</button></a>
+                <img src={logo} alt='Logo' style={{width: "300px", padding: "20px" }} />
+                <h1 style={{ display: "inline-block", paddingLeft: "100px", position: "relative", top:"30px" }}>Mentor Portal</h1>
+                <a href="https://www.villagebookbuilders.org/giftabook/" style={{ display: "inline-block", paddingLeft: "100px", position: "relative", top:"27px" }}><button>Donate</button></a>
                 {
                     props.isAuthenticated ?
-                    <a href="/login" style={{ display: "inline-block", paddingLeft: "100px" }}><button>Sign Out</button></a> : <p style={{ display: "inline-block" }}></p>
+                    <a href="/login" style={{ display: "inline-block", paddingLeft: "100px" }}><button>Sign Out</button></a> : <p style={{ display: "inline-block", position: "relative", top:"27px" }}></p>
                 }
                 <br />
                 <hr style={{ border: "2px solid #FF914D" }} />
