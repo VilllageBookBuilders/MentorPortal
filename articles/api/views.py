a8393f7dedfb06f6fb3c5fba534133cf79ee3edf
Ed Ringger <edringger@gmail.com>
2020-07-13 23:01:01 -0700

    adjustments to include google auth

--- articles/api/views.py
+++ articles/api/views.py
@@ -44,3 +44,12 @@ class ArticleDeleteView(DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = (permissions.IsAuthenticated, )
+
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from rest_auth.registration.views import SocialLoginView
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
