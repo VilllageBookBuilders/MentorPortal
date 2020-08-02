078a081441b452d2f77fced2026a5c9e00bc95d1
Ed Ringger <edringger@gmail.com>
2020-08-01 19:04:43 -0700

    fix routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Signup2 from "./components/Signup2";
 import Profile from "./components/Profile";
-import BasicBooking from "./components/BasicBooking";
-import CalendarDemo from "./components/Booking/BookingComponent";
+// import BasicBooking from "./components/BasicBooking";
+// import CalendarDemo from "./components/Booking/BookingComponent";
 import MasterForm from "./components/Registration";
 import Booking from "./components/Booking";
 import { SigninGoogle } from "./components/SigninGoogle";
@@ -22,8 +22,8 @@ const BaseRouter = () => (
     <Route exact path="/signup/" component={Signup} />
     <Route exact path="/signup2/" component={Signup2} />
     <Route exact path="/profile/" component={Profile} />
-    <Route exact path="/booking/" component={BasicBooking} />
-    <Route exact path="/calendar/" component={CalendarDemo} />
+    {/* <Route exact path="/booking/" component={BasicBooking} /> */}
+    {/* <Route exact path="/calendar/" component={CalendarDemo} /> */}
     <Route exact path="/registration/" component={MasterForm} />
     <Route exact path="/booking/" component={Booking} />
     <Route exact path="/signin/" component={SigninGoogle} />
